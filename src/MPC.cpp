9f02e181659071c250ed1712d7c1a7341bbd5882
Andreas Graf <ag-online@quanxinquanyi.de>
2018-03-30 16:34:40 +0200

    initial commit

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -2,12 +2,34 @@
 #include <cppad/cppad.hpp>
 #include <cppad/ipopt/solve.hpp>
 #include "Eigen-3.3/Eigen/Core"
+#include <limits>
 
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+
+// Using size_t seems strange, we want semantic types
+// and size_t is the sizeof an object, not an offset;
+//
+typedef unsigned long  ulong;
+ulong const N = 10;       // Initial experimental value
+double const  dt = 0.1;    // Initial experimental value
+
+const ulong x_offs = 0;
+const ulong y_offs = x_offs + N;
+const ulong psi_offs = y_offs+N;
+const ulong v_offs = psi_offs +N;
+const ulong cte_offs = v_offs+N;
+const ulong epsi_offs = cte_offs+N;
+const ulong d1_offs = epsi_offs+N;
+const ulong a_offs = d1_offs+(N-1);
+
+
+const double angle_constraint = 0.436332; // Constraint for steering angle
+const double acc_constraint = 1.0; // Constraint for acceleration
+
+const ulong n_input_params = 6;
+const ulong n_actuators = 2;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -47,19 +69,16 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
-  // TODO: Set the number of model variables (includes both states and inputs).
-  // For example: If the state is a 4 element vector, the actuators is a 2
-  // element vector and there are 10 timesteps. The number of variables is:
+  // calculate constants
+  // Refactored to not use any hard coded constants.
   //
-  // 4 * 10 + 2 * 9
-  size_t n_vars = 0;
-  // TODO: Set the number of constraints
-  size_t n_constraints = 0;
+  ulong const n_vars = (n_input_params*N)+n_actuators*(N-1);
+  ulong const n_constraints = n_input_params*N;
 
   // Initial value of the independent variables.
   // SHOULD BE 0 besides initial state.
   Dvector vars(n_vars);
-  for (int i = 0; i < n_vars; i++) {
+  for (unsigned int i = 0; i < n_vars; i++) {
     vars[i] = 0;
   }
 
@@ -67,14 +86,58 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   Dvector vars_upperbound(n_vars);
   // TODO: Set lower and upper limits for variables.
 
+  vars[x_offs] = state[0];
+  vars[y_offs] = state[1];
+  vars[psi_offs] = state[2];
+  vars[v_offs] = state[3];
+  vars[cte_offs] = state[4];
+  vars[epsi_offs] = state[5];
+
+  // Initialize the bounds in a portable way for all platforms
+  //
+  for(unsigned int i = 0; i < d1_offs; i++) {
+      vars_lowerbound[i] = std::numeric_limits<double>::lowest();
+      vars_upperbound[i] = std::numeric_limits<double>::max();
+  }
+
+  // The upper and lower limits of delta are set to -25 and 25
+  // (Values taken from the project lesson)
+  for (unsigned int i = d1_offs; i < a_offs; i++) {
+    vars_lowerbound[i] = - angle_constraint;
+    vars_upperbound[i] = angle_constraint;
+  }
+
+  // Experimental values
+  // AGR: Check for maximum acceleration and jerk
+  //
+  for (unsigned int i = a_offs; i < n_vars; i++) {
+    vars_lowerbound[i] = -acc_constraint;
+    vars_upperbound[i] = acc_constraint;
+  }
+
   // Lower and upper limits for the constraints
   // Should be 0 besides initial state.
   Dvector constraints_lowerbound(n_constraints);
   Dvector constraints_upperbound(n_constraints);
-  for (int i = 0; i < n_constraints; i++) {
+  for (unsigned int i = 0; i < n_constraints; i++) {
     constraints_lowerbound[i] = 0;
     constraints_upperbound[i] = 0;
   }
+
+  constraints_lowerbound[x_offs] = state[0];
+  constraints_lowerbound[y_offs] = state[1];
+  constraints_lowerbound[psi_offs] = state[2];
+  constraints_lowerbound[v_offs] = state[3];
+  constraints_lowerbound[cte_offs] = state[4];
+  constraints_lowerbound[epsi_offs] = state[5];
+
+  constraints_upperbound[x_offs] = state[0];
+  constraints_upperbound[y_offs] = state[1];
+  constraints_upperbound[psi_offs] = state[2];
+  constraints_upperbound[v_offs] = state[3];
+  constraints_upperbound[cte_offs] = state[4];
+  constraints_upperbound[epsi_offs] = state[5];
+
 
   // object that computes objective and constraints
   FG_eval fg_eval(coeffs);
@@ -96,6 +159,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
   // Change this as you see fit.
   options += "Numeric max_cpu_time          0.5\n";
+  // AGR: Change CPU TIME!
 
   // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
@@ -117,5 +181,10 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   //
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
-  return {};
+  std::vector<double> result = {solution.x[d1_offs], solution.x[a_offs] };
+  for (unsigned int i = 0; i < N; ++i) {
+    result.push_back(solution.x[x_offs + i]);
+    result.push_back(solution.x[y_offs + i]);
+  }
+  return result;
 }
